0ae1df093e9237f245d3a58dbdb6618819dfec82
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-06 09:34:41 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	/* Using a new print to the console */
-	fprintf(stderr, "and that piece of art is useful\" \
-	- Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" "
+	"- Dora Korpar, 2015-10-19\n");
 	return (1);
 }
