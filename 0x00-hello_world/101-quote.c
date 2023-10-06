b02ee807aea24c657d6d5f16454652f04ae49298
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-05 20:46:23 -0700

    Genius

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Using a new print to the console */
+	fprintf(stderr, "and that piece of art is useful\" \
+	- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
