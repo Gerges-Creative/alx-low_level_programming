246e81fcf61b06849c615c98aa19d91cb3f38136
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-05 18:56:02 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* printing the console using printf */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
