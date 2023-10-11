a8740725d3f9a70e2d9e3be2b0c3394b977d0333
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-10 20:09:14 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of a number
+ *
+ * @n: integer argument
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		_putchar(n * -1);
+	}
+	else
+	{
+		_putchar(n);
+	}
+
+	return (0);
+}
