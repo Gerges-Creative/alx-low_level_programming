cb23ff5090e76b9e8999c370d0888d272c024b75
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-11 13:59:32 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: No return value
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			z = x * y;
+			_putchar(z + '0');
+
+			if (z < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (z > 9 && z < 81)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
