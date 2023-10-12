09b629bbb078e4fca72f5e88ece460e7e544130b
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-11 17:55:13 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - calculate the sum of two integers
+ *
+ * @x: a parameter that take an integer
+ * @y: a parameter that take an integer
+ *
+ * Return: The result of the addition
+ */
+int add(int x, int y)
+{
+	int z = x * y;
+
+	return (z);
+}
