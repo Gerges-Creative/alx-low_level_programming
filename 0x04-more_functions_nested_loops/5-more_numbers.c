7516b8a7dce8104f81a0e76bf470ca10e636b289
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-12 11:47:47 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print 10 times the numbers from 0 - 14
+ *
+ * Return: No return value
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar('1');
+			}
+
+			_putchar((j % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
