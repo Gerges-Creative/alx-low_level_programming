186357d2e1a726a781444878a33108307dae659a
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-12 10:39:58 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks if a letter is uppercase or not
+ *
+ *
+ *
+ * Return: No return value
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('0');
+}
