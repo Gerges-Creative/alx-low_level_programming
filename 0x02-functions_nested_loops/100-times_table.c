d12ba5584655c318cb4685d74fb4933f8b3f577d
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-11 19:23:11 -0700

    Print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,9 +36,9 @@ void print_times_table(int n)
 					_putchar((z / 10) + '0');
 					_putchar((z % 10) + '0');
 				}
-				else if (z > 99)
+				else if (z >= 100)
 				{
-					_putchar((z / 10) + '0');
+					_putchar(((z / 100) % 10) + '0');
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
 				}
