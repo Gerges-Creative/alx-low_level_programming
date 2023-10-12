c4a9483306c429231ec37bb36d694498369fb6a2
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-11 17:15:08 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,21 +11,24 @@ void times_table(void)
 
 	for (x = 0; x < 10; x++)
 	{
+		_putchar('0');
+
 		for (y = 0; y < 10; y++)
 		{
 			z = x * y;
-			_putchar(z);
+
+			_putchar(',');
+			_putchar(' ');
 
 			if (z < 10)
 			{
-				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
+				_putchar(z + '0');
 			}
-			else if (z > 9 && z < 81)
+			else if (z > 9)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
 			}
 		}
 
