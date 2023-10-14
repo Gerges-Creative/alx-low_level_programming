b764abc22c120130b1f885363958505d6b5aaf85
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-13 19:51:40 -0700

    Print any number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - prints a given number to the function
+ *
+ * @n: an integer
+ *
+ * Return: No return value
+ */
+void print_number(int n)
+{
+	int i;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+	}
+	else if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+
+	do {
+		i = n % 10;
+		n = n / 10;
+
+		_putchar(i + '0');
+
+	} while (n > 0);
+
+	_putchar('\m');
+}
