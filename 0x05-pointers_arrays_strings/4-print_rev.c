08bb5c6f3930dd4656b333d44186900099412958
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-16 18:48:09 -0700

    Rverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - pritns a stirng in reverse order
+ *
+ * @s: Pointer points to a string of type char
+ *
+ * Return: No return value
+ */
+void print_rev(char *s)
+{
+	char *length = s;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	while (length > s)
+	{
+		_putchar(*length);
+		length--;
+	}
+
+	_putchar('\n');
+}
