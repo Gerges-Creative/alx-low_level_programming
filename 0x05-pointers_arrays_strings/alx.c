a49e1e1c2d51a567df67fcc137a06d774423b1ca
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-17 05:03:52 -0700

    Puts 2

--- 0x05-pointers_arrays_strings/alx.c
+++ 0x05-pointers_arrays_strings/alx.c
@@ -9,7 +9,10 @@ int main(void)
 {
     char *str;
 
-    str = "0123456789";
+    str = "0123456789 Lrmismdlrstae,cnettraiicn lt e oeumdtmo niiutu aoee ooemga\
+           aiu.U nma ii eim usnsrdeectto lac aoi iiu lqi xe omd osqa.Di ueiuedl	\
+           ri erhnei nvlpaevltes ilmdlr ufga ul aitr xetu itocea uiaa o riet u \
+           ti up u fii eeutmli nmi s aou.";
     puts2(str);
     return (0);
 }
