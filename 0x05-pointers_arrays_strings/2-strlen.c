f327bfb10c02c5157a7aa2f43815a6e89c926225
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-16 17:27:37 -0700

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#inlcude "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: Pointer
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i <= *s; i++)
+	{
+	}
+
+	return (i + 1);
+}
