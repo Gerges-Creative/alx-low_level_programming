62b8a88041f2addddb772efe9661403b1210e08d
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-16 19:52:06 -0700

    Rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - print an array of char but reverse its elements so a[0] to [10]
+ *
+ * @s: Pointer
+ *
+ * Return: No return value
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int i = 0;
+	char *rev = s;
+
+	while (s[length] != '\0')
+	{
+		length++
+	}
+
+	while (length >= i)
+	{
+		rev[i] = s[length];
+		_putchar(rev[i]);
+		i++;
+		length--;
+	}
+
+	_putchar('\n');
+}
