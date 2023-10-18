ee01f4698da146c0d065bf849240efcdb52db5bf
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-18 16:28:10 -0700

    Strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy an n characters from string src to dest
+ *
+ * @dest: the pointer that is src string is copied to
+ * @src: the pointer that takes the string
+ * @n: integer thar refers to the number of characters
+ *
+ * Return: return dest
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+	int i;
+
+	for (i = 0; i < 0 && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
