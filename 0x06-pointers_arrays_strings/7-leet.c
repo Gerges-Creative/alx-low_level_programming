2cebfad8d1d7ff9ad938b56c5b854ef888083837
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-19 16:41:24 -0700

    LEET 7331

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,33 @@
 #include "main.h"
 
 /**
- * 
+ * *leet - an encryption code to change the letter a, e, o, t, l and their
+ * uppercase version to 4, 3, 0, 1, 7 respectively
+ *
+ * @c: pointer char that checks string
+ *
+ * Return: return the encrypted string
+ */
+char *leet(char *c)
+{
+	char lower[] = {'a', 'e', 'o', 't', 'l'};
+	char upper[] = {'A', 'E', 'O', 'T', 'L'};
+	char num[] = {'4', '3', '0', '7', '1'};
+
+	int i = 0, j;
+
+	while (c[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (c[i] == lower[j] || c[i] == upper[j])
+			{
+				c[i] = num[j];
+			}
+		}
+
+		i++;
+	}
+
+	return (c);
+}
