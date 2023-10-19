599cb37b9a5149965781db57c9d78779bfe9a55a
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-19 11:11:22 -0700

    Caps Words

--- 0x06-pointers_arrays_strings/alx.c
+++ 0x06-pointers_arrays_strings/alx.c
@@ -8,13 +8,14 @@
  */
 int main(void)
 {
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what\
-                  comes.\nhello world! hello-world 0123456hello world\thello\
-                  world.hello world\n";
-    char *ptr;
+	char str[] = "Expect the best. Prepare for the worst. Capitalize on what"
+		"comes.\nhello{world}!hello-world 0123456hello world\thello"
+		"world.(hello) world\n"
+		"!expect,the.best prepare\tfor;the?worst\"capitalize(on)what{comes}hello world";
+	char *ptr;
 
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
+	ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
 }
