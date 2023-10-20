7a294ea49a74d442951c59d743386e73f8f24548
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-19 17:07:17 -0700

    ROT13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *rot13 - using the encryption encoding rot12
+ *
+ * @rot: pointer char takes a string to encode
+ *
+ * Return: return the encoded string
+ */
+char *rot13(char *rot)
+{
+	char lower[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i = 0, j;
+
+	while (rot[i] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (rot[i] == lower[j])
+			{
+				rot[i] = rot13[j];
+			}
+		}
+
+		i++;
+	}
+
+	return (rot);
+}
