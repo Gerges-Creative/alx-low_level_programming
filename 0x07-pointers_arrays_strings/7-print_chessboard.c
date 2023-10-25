9a3dde8aa515867bd4995722b1fa9088a45e621f
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-24 18:27:34 -0700

    Chessborad2

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -15,14 +15,7 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			if (a[i][0] == ' ')
-			{
-				break;
-			}
-			else
-			{
-				_putchar(a[i][j]);
-			}
+			_putchar(a[i][j]);
 		}
 
 		_putchar('\n');
