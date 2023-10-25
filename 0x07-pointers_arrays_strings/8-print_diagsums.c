5dfb52d601b3a99bd78cad5235a56289b9e9c3c7
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-24 20:20:09 -0700

    Sum Diagonals of square matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,14 +14,14 @@ void print_diagsums(int *a, int size)
 	int i, j;
 	int sumDia1 = 0, sumDia2 = 0;
 
-	for (i = 0;i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		sumDia1 += a[i][i];
+		sumDia1 = sumDia1 + *(a + i * size + i);
 	}
 
-	for (j = size - 1; j >= 0; j--)
+	for (j = 1; j <= size; j++)
 	{
-		sumDia2 += a[j][j];
+		sumDia2 = sumDia2 + *(a + j * size - j);
 	}
 
 	printf("%d, %d\n", sumDia1, sumDia2);
