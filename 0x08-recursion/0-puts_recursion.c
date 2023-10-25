2436091565c3e5fecc471d47287dd4caf6d1a9ac
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-25 15:13:57 -0700

    First Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,9 @@
 #include "main.h"
 
-/**
- * puts_recursion - printing a string using recursion
- *
- * @s: string
- *
- * Return: no return value
- */
+
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
