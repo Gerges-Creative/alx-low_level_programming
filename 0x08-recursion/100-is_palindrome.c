d5de3de41314872645ccad9746f413060c3d5552
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-25 20:59:09 -0700

    anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * is_palindrome - check if a string is palindrome or not
+ * palindrome means that the string remains the same on reading
+ * from both ends.
+ *
+ * @s: string
+ * Return: 1 if it's palindrome else 0
+ */
+int is_palindrome(char *s)
+{
+	return (1);
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare two strings together
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if identical otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (1);
+}
