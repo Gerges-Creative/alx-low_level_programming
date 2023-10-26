a448d264bfdec7cd299f7452b714744c86dbda84
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-26 16:23:11 -0700

    Wildcard

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * _strcmp - cmp the two strings
+ * @a: string
+ * @b: string
+ * Return: 1 or 0
+ */
+int _strcmp(char *a, char *b)
+{
+	if (*a == '\0' && *b == '\0')
+	{
+		return (1);
+	}
+
+	if (*a == *b)
+	{
+		return (_strcmp(++a, ++b));
+	}
+
+	return (0);
+}
+
+/**
  * wildcmp - compare two strings together
  * @s1: first string
  * @s2: second string
@@ -8,5 +29,5 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (1);
+	return (_strcmp(s1, s2));
 }
