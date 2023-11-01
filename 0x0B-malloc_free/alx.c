0d14fd7d69bccfda8739f08bf152c9d353a77e14
Gerges-Creative <gerges.zechariah@gmail.com>
2023-10-31 20:59:10 -0700

    Grid

--- 0x0B-malloc_free/alx.c
+++ 0x0B-malloc_free/alx.c
@@ -3,21 +3,32 @@
 #include <stdlib.h>
 
 /**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void print_grid(int **grid, int width, int height)
+{
+    int w;
+    int h;
+
+    h = 0;
+    while (h < height)
+    {
+        w = 0;
+        while (w < width)
+        {
+            printf("%d ", grid[h][w]);
+            w++;
+        }
+        printf("\n");
+        h++;
+    }   
+}
+
+/**
  * main - check the code for ALX School students.
  *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s;
-
-    s = _strdup("ALX SE");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
-}
