b2e41ab493b913e37fd8415b5f22529c2baea897
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-01 19:04:45 -0700

    Concatenate Args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * argstostr - takes function arguments and concatenate them together
+ * @ac: number of args
+ * @av: pointer to args
+ * Return: pointer to the new string or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+
+		while (av[i][j] != '\0')
+		{
+			str[k] = av[i][j];
+			k++, j++;
+		}
+
+		str[k] = '\n';
+		k++;
+	}
+
+	str[k] = '\0';
+
+	str = (char *)malloc(k * sizeof(char));
+
+	if (str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+
+	return (str);
+}
