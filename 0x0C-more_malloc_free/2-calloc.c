316c34e158f2f887023c7affd6043551aab5825c
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-02 17:05:12 -0700

    Custom Calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _calloc - create a calloc function
+ * @nmemb: number of elements
+ * @size: size of the element
+ * Return: pointer to new allocated memory or NULL if it fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if(ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
