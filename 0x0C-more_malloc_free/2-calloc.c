3f24a8eb7c2987088f43c938664b2dfd8bcc523b
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-02 17:33:00 -0700

    Custom Calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,8 +21,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	for (i = 0; i < nmemb; i++)
-		ptr[i * size] = 0;
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
 
 	return (ptr);
 }
