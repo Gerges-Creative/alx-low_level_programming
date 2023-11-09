b24c83d6ec590ec49907e0cd690cf95cde9fe871
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-08 17:08:48 -0800

    Array of Functions

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - an array of function that executes every function 
+ * whenever called
+ *
+ * @array: an array of integers
+ * @size: size of the array
+ * @action: pointer to function
+ * Retrun: No return
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
