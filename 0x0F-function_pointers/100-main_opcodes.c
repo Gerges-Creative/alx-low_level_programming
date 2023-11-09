0a4cfb9ea85e3f0b202de380eea3cc4e035af499
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-09 05:47:52 -0800

    OPCODE

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the opcodes of the main function
+ * @argc: number of args
+ * @argv: arg vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int bytes;
+	char *mainAddress = (char *) main;
+
+	bytes = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	while (bytes > 0)
+	{
+		printf("%02hhx", *mainAddress);
+
+		if (bytes)
+			printf(" ");
+
+		mainAddress++;
+		bytes--;
+	}
+
+	printf("\n");
+
+	return (0);
+}
