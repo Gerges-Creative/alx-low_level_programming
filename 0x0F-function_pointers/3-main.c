b6c1d72ec85189a5e87d944be141951bcff8d38b
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-08 20:28:22 -0800

    Creating Functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,13 +9,13 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
+	int num1, num2, result;
 	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
+		exit (98);
 	}
 
 	operation = get_op_func(argv[2]);
@@ -23,13 +23,14 @@ int main(int argc, char *argv[])
 	if (operation == NULL)
 	{
 		printf("Error\n");
-		return (99);
+		exit (99);
 	}
 
 	 num1 = atoi(argv[1]);
 	 num2 = atoi(argv[3]);
 
-	 printf("%d\n", operation(num1, num2));
+	 result = operation(num1, num2);
+	 printf("%d\n", result);
 
 	return (0);
 }
