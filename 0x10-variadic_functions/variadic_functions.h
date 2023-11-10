4d37e614996979bfb6a6bd51543ea0c7b9b96137
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-09 19:55:21 -0800

    Print All

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,9 +5,20 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct dt - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
+typedef struct all
+{
+	char specefier;
+	void (*func)(va_list);
+} dataType;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-
+void print_all(const char * const format, ...);
 
 #endif
