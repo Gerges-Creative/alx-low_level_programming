3a3af52e86507c92bd5aa96c883143c9890f79bf
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-09 17:53:59 -0800

    Print String

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (strn != NULL)
 			printf("%s", strn);
 		else
-			printf("nil");
+			printf("(nil)");
 
 		if (separator && i < n - 1)
 			printf("%s", separator);
