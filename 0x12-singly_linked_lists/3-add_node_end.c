1958f42f8095a9d87a84b365c805b1f35ae01780
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-18 06:11:14 -0800

    Tail of Node

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add a node at the end of the list
+ * @head: pointer to the current node which is the tail of the linked list
+ * @str: pointer to string
+ * Return: address of head
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	size_t count = 0;
+	list_t *tail;
+
+	tail = malloc(sizeof(list_t));
+	if (tail == NULL)
+		return (NULL);
+
+	tail->str = strdup(str);
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+
+	tail->len = count;
+	tail->next = *head;
+	*head = NULL;
+
+	return (*head);
+}
