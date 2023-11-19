5ceb5bcb66523724066a8564f3462aa5476861fe
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-18 22:39:16 -0800

    u

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,12 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		h = h->next;
 		if (h->str == NULL)
 			printf("[0] (nill)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 
-
+		h = h->next;
 		count++;
 
 	}
