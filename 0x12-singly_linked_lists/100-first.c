462c69b1a064d1345c5c1eef4f2d386d97eeb4fe
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-19 05:38:07 -0800

    Startup function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * Apply the constructor attribute to startup() so that it
+ *   is executed before main()
+ */
+void startup (void) __attribute__ ((constructor));
+
+/**
+ * startup - prints a string before the main function is executed
+ * Return: NO return
+ */
+void startup(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
