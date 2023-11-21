b1a4c5ff7fd86b2cb0955fbe5862ef3a79f9dc63
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-21 06:38:39 -0800

    u

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,18 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *temp, *current;
 
 	if (*head == NULL)
 		return;
 
-	while (*head != NULL)
+	current = *head;
+
+	while (current != NULL)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		temp = current;
+		current = current->next;
+		free(temp);
 	}
 
 	*head = NULL;
