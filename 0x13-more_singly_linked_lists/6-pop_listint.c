53df05723b5839e1f0591b2785116bafd08119de
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-21 06:51:04 -0800

    u

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,13 +9,16 @@
 int pop_listint(listint_t **head)
 {
 	int n;
+	listint_t *h;
 
 	if (*head == NULL)
 		return (0);
 
 	n = (*head)->n;
 
+	h = *head;
 	*head = (*head)->next;
+	free(h);
 
 	return (n);
 }
