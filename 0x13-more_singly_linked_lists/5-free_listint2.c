b7db7dec6b4c753100e28ab1071c13af3e3e0d73
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-21 06:42:45 -0800

    u

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the nodes of the linked list and then sets the
+ * head to NULL
+ * @head: pointer to the head of the linked list
+ * Return: No return
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *current;
+
+	if (*head == NULL)
+		return;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+
+	*head = NULL;
+}
