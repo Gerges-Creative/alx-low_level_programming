f26e5b27640e042fae3d3fab3510ba2d7acdbe70
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-22 05:24:05 -0800

    u

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *delindex, *temp1, *temp2;
 	unsigned int count = 0;
 
-	if (!head || (index > 0 && !*head))
+	if (!*head || (index > 0 && !*head))
 		return (-1);
 
 	if (index == 0)
