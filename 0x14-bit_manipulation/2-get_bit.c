6cd053b6c27dc1ebd6e713746a89855cb79569de
Gerges-Creative <gerges.zechariah@gmail.com>
2023-11-25 10:14:11 -0800

    Update error handling

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,6 +10,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask = 1;
 
+	if (index >= sizeof(unsigned long int ) * 8)
+		return (-1);
+
 	if (n == 0)
 	{
 		return (0);
@@ -20,5 +23,5 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n & mask)
 		return (1);
 	else
-		return(0);
+		return (0);
 }
