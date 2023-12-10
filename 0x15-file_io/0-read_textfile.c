27cb50c9d7e9078bd19f039d30f7d4de79fccb6f
Gerges-Creative <gerges.zechariah@gmail.com>
2023-12-10 11:35:39 -0800

    Read

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a certain size and prints to std output
+ * @filename: file to read from
+ * @letters: size to read
+ * Return: actual size read and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+
+	if (filename == NULL)
+        return 0;
+
+	int file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return 0;
+
+	*buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(file_descriptor);
+		return 0;
+	}
+
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(file_descriptor);
+		free(buffer);
+		return 0;
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
+	{
+		close(file_descriptor);
+		free(buffer);
+		return 0;
+	}
+
+	close(file_descriptor);
+	free(buffer);
+
+	return bytes_read;
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This the File I/O project readme
