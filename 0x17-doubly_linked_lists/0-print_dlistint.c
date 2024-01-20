e98d95526ba6dfc4285f817947024770c231ad39
Gerges-Creative <gerges.zechariah@gmail.com>
2024-01-20 10:30:00 -0800

    Print Doubly list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,13 +9,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *temp;
-	size_t i;
+	const dlistint_t *temp;
+	int i;
+
 	temp = h;
 
 	while (temp != 0)
 	{
-		printf("%i\n", temp->n);
+		printf("%d\n", temp->n);
 		temp = temp->next;
 		i++;
 	}
