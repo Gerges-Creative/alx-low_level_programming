d25f13e74e63f0f0d4b1df9d568a4d7e4324578a
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-09 12:15:40 -0800

    Add element ot HT

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,8 +11,8 @@ hash_node_t *create_node(const char *key, const char *value)
 	hash_node_t *node;
 
 	node = malloc(sizeof(hash_node_t));
-	node = malloc(strlen(key) + 1);
-	node = malloc(strlen(value) + 1);
+	node->key = malloc(strlen(key) + 1);
+	node->value = malloc(strlen(value) + 1);
 	if (node == NULL || node->key == NULL || node->value == NULL)
 		return (NULL);
 
@@ -33,16 +33,33 @@ hash_node_t *create_node(const char *key, const char *value)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node, *current_node;
+	hash_node_t *node, *current_node, *temp;
 
-	if (*key == '\0')
+	if (ht == NULL)
+		return (0);
+
+	if (key == NULL || *key == '\0')
 		return (0);
 
 	node = create_node(key, value);
+	if (node == NULL)
+		return (0);
 
-	index = key_index((const unsigned char*) key, ht->size);
+	index = key_index((const unsigned char *) key, ht->size);
 
 	current_node = ht->array[index];
+
+	temp = current_node;
+	while (temp != NULL)
+	{
+		if (strcmp(key, temp->key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
 
 	if (current_node == NULL)
 	{
