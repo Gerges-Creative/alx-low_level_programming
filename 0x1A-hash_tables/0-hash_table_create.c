a06e914937475807b180635ea256d878c913ee06
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-09 06:38:53 -0800

    HashTables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,17 +8,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	hash_node_t **array;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
+	array = calloc(size, sizeof(hash_node_t));
+	if (array == NULL)
+		return (NULL);
+
+	table->array = array;
 	table->size = size;
 
-/**
- *	table->array = calloc(table->size, sizeof(hash_node_t));
- *	if (table->array == NULL)
- *		return (NULL);
- */
 	return (table);
 }
