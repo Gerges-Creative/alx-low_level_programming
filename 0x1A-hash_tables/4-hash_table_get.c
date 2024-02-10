0d75a5bbd8264e255d045cd917681f6ec5718903
Gerges-Creative <gerges.zechariah@gmail.com>
2024-02-10 06:47:52 -0800

    Retrieve a Value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - a function that retrieves a value associated with a key
+ * @ht: is the hash table which will be looked into
+ * @key: is the key to be matched for to get its value
+ * Return: the value associated with the key element or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	index = key_index(key, ht->size);
+
+	node = ht->array[index];
+
+	if (node == NULL || key == NULL)
+		return (NULL);
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			if (node->value != NULL)
+				return (node->value);
+			else
+				return ("(null)");
+		}
+		node = node->next;
+	}
+}
