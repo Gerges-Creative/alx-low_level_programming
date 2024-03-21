b1e5e235a9ed7d2312912ee0ae67b0ebe98845c2
Gerges-Creative <gerges.zechariah@gmail.com>
2024-03-21 10:50:21 -0700

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,7 +6,8 @@
 
 def island_perimeter(grid):
     """
-    Function to calculate the perimeter of the grid
+    Function to calculate the perimeter of the grid.
+    Noting that any where outside the grid is water.
     """
     perimeter = 0
     lenI = len(grid)
@@ -15,13 +16,13 @@ def island_perimeter(grid):
         lenJ = len(grid[i])
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if j != lenJ - 1 and grid[i][j + 1] == 0:
-                    perimeter += 1
-                if grid[i][j - 1] == 0:
-                    perimeter += 1
-                if i != lenI - 1 and grid[i + 1][j] == 0:
-                    perimeter += 1
-                if grid[i - 1][j] == 0:
-                    perimeter += 1
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1 # Top row
+                if i == (lenI - 1) or grid[i + 1][j] == 0:
+                    perimeter += 1 # Bottom row
+                if j == (lenJ - 1) or grid[i][j + 1] == 0:
+                    perimeter += 1 # Right side
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1 # Left side
 
     return perimeter
